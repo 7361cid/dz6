763f54a18d529d18eb98bcc0ad33ab2165bcb795
Chernov.Ilia <ilia.chernov@infotecs.ru>
2022-05-05 18:50:02 +0700

    Начало реализации регистрации

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import CustomUser
+
+# Создаем здесь представления.
+
+
+def home(request):
+    return render(request, "home.html")
+
+
+class SignUp(CreateView):
+    model = CustomUser
+    template_name = 'registration\signup.html'
+    fields = '__all__'
+
