379db35430f7505fb750b36171c0d9e32fc7b38e
Chernov.Ilia <ilia.chernov@infotecs.ru>
2022-05-06 11:43:57 +0700

    Регистрация ОК

--- mysite/users/views.py
+++ mysite/users/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
+from django import forms
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
-
+from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 
 # Создаем здесь представления.
 
 
-def home(request):
-    return render(request, "home.html")
+class RegisterForm(UserCreationForm):
+    class Meta:
+        model = CustomUser
+        fields = ("username", "email", "profile_pic")
 
 
 class SignUp(CreateView):
-    model = CustomUser
+    form_class = RegisterForm
     template_name = 'registration\signup.html'
-    fields = '__all__'
-
+    success_url = reverse_lazy('login')
