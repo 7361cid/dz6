613a7469ace79aa94ad5950df757551688c1b6ff
Chernov.Ilia <ilia.chernov@infotecs.ru>
2022-05-13 19:10:47 +0700

    Настроил макет верстки

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -71,8 +71,8 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'mysite.wsgi.application'
-LOGIN_REDIRECT_URL = "/"
-LOGOUT_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/users"
+LOGOUT_REDIRECT_URL = "/users"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
